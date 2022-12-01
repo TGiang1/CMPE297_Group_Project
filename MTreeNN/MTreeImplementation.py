648091c4a7a7fc9ae311153e3cd38bb508e80a85
Tommy Giang <tommy.giang@sjsu.edu>
2022-11-30 17:30:37 -0800

    Initial Test of MTree
    Initial Test of MTree

---
+++ MTreeNN/MTreeImplementation.py
@@ -0,0 +1,16 @@
+import mtree
+import numpy as np
+
+def d_int(x, y):      # define a distance function for numbers. euclidean for 3 dimensions
+    return ((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2)**(0.5)
+
+queriesArray = np.random.uniform(0, 5, size=(3, 3))
+queriesArray = np.round(queriesArray, 2)
+
+tree = mtree.MTree(d_int, max_node_size=4)  
+
+tree.add(list(queriesArray[0]))
+tree.add_all([list(queriesArray[1]), list(queriesArray[2])])
+a = tree.search([1,1, 1])
+
+print(list(a))
